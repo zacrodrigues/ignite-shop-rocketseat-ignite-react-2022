0d9279b157072fd5e878cd29b799945f6007b0a9
Zac Rodrigues <rodrigues.contano@gmail.com>
2023-02-17 18:42:44 -0300

    Botão de comprar

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,8 @@
+import axios from "axios"
 import { GetStaticPaths, GetStaticProps } from "next"
 import Image from "next/image"
 import { useRouter } from "next/router"
+import { useState } from "react"
 import Stripe from "stripe"
 
 import { stripe } from "../../lib/stripe"
@@ -19,8 +21,23 @@ interface ProductProps {
 }
 
 export default function Product({ product }: ProductProps) {
-  function handleBuyProduct() {
-    console.log(product.defaultPriceId)
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] = useState(false)
+
+  async function handleBuyProduct() {
+    setIsCreatingCheckoutSession(true)
+
+    try {
+      const response = await axios.post('/api/checkout', {
+        priceId: product.defaultPriceId
+      })
+
+      const { checkoutUrl } = response.data
+
+      window.location.href = checkoutUrl
+    } catch (err) {
+      alert('Falha ao redirecionar ao checkout!')
+      setIsCreatingCheckoutSession(false)
+    }
   }
 
   return (
@@ -35,7 +52,12 @@ export default function Product({ product }: ProductProps) {
 
         <p>{product.description}</p>
 
-        <button onClick={handleBuyProduct}>Comprar agora</button>
+        <button
+          disabled={isCreatingCheckoutSession}
+          onClick={handleBuyProduct}
+        >
+          Comprar agora
+        </button>
       </ProductDetails>
     </ProductContainer>
   )
